77111075ad4316a358f5b5089d91e5f4fe3a6e56
eldray <emk.appiah@gmail.com>
2026-06-05 20:56:34 +0000

    resolve backend types

--- backend/src/modules/delivery/delivery.service.ts
+++
@@ -1,1 +0,0 @@
-// Updated with NewbornRecord integration
--- backend/src/modules/family-planning/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './familyPlanning.controller';
-export * from './familyPlanning.service';
-export * from './familyPlanning.module';
